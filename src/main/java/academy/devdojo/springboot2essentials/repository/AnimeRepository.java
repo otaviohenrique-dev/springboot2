e5f69c3079b83fcbee992137db79257c9bc8d257
Lorenzo Henrique <educsalaangra@gmail.com>
2024-05-24 15:08:07 -0300

    First Commit

---
+++ src/main/java/academy/devdojo/springboot2essentials/repository/AnimeRepository.java
@@ -0,0 +1,7 @@
+package academy.devdojo.springboot2essentials.repository;
+
+import academy.devdojo.springboot2essentials.domain.Anime;
+
+public interface AnimeRepository {
+    public Anime listAll();
+}
