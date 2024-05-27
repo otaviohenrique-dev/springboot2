d0f38d8d0cbdcf576fd954eed8e21ad99d4fce60
Lorenzo Henrique <educsalaangra@gmail.com>
2024-05-27 12:39:20 -0300

    Implement methods POST

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -7,10 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -30,5 +28,9 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> getById (@PathVariable Long id){
         return ResponseEntity.ok(animeService.getById(id));
+    }
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
