cb9c7242c9adc5e6c3141bfdca6eba3665740da7
Lorenzo Henrique <educsalaangra@gmail.com>
2024-05-27 12:39:20 -0300

    Implement methods POST

--- src/main/java/academy/devdojo/springboot2essentials/services/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/services/AnimeService.java
@@ -3,13 +3,20 @@ package academy.devdojo.springboot2essentials.services;
 import academy.devdojo.springboot2essentials.domain.Anime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1L, "Naruto"), new Anime(2L, "DBZ"), new Anime(3L, "TT444567686867dgdfgdfgdfgdfg67"));
+    private static List<Anime> animes;
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Naruto"), new Anime(2L, "DBZ"), new Anime(3L, "TT444567686867dgdfgdfgdfgdfg67")));
+    }
     public List<Anime> listAll (){
         return animes;
     };
@@ -19,5 +26,11 @@ public class AnimeService {
                 .findAny()
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime Not Found"));
 
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000));
+        animes.add(anime);
+        return anime;
     }
 }
